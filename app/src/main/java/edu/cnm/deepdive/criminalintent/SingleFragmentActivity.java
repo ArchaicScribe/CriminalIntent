15bb54577cdaac64821372857c307e86e13df0fc
Alex Rauenzahn <xandermv@yahoo.com>
2019-02-28 10:21:02 -0700

    Fixed issues with SingleFragmentActivity.

--- app/src/main/java/edu/cnm/deepdive/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/edu/cnm/deepdive/criminalintent/SingleFragmentActivity.java
@@ -12,7 +12,7 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 
 
   @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_fragment); // set the activity view to
     // be inflated.
@@ -22,15 +22,7 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     if (fragment == null) {
       fragment = createFragment();
-
       fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-            /* .add ... line of code creates and commits a fragment transaction.
-        Fragment transactions are used to add, remove, attach, detach, or
-        replace fragments in the fragment list.  They are the heart of how you
-        use fragments to compose and recompose screens at runtime. The Fragment-
-        Transaction class uses a fluent interface and returns a FragmentTrans-
-        action so that the fragments can be chained together.
-        */
 
     }
   }
